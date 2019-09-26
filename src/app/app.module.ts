a4821d41012f9ff9e2f371d1e05a97a60c872a24
Johnny To <johnny.to@outlook.com>
2019-09-26 16:14:39 -0400

    Added navigation bar

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,12 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { TopNavigationBarComponent } from './top-navigation-bar/top-navigation-bar.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    TopNavigationBarComponent
   ],
   imports: [
     BrowserModule
