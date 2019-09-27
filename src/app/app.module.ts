643897a767244903a19d08b10f0aa44c0c387e65
Johnny To <johnny.to@outlook.com>
2019-09-27 16:13:41 -0400

    Added several new pages and setup Routing Module for URL navigation

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,37 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { RouterModule } from '@angular/router';
 
 import { AppComponent } from './app.component';
 import { TopNavigationBarComponent } from './top-navigation-bar/top-navigation-bar.component';
+import { AboutMeComponent } from './about-me/about-me.component';
+import { HomepageComponent } from './homepage/homepage.component';
+import { ResumeComponent } from './resume/resume.component';
+import { PortfolioComponent } from './portfolio/portfolio.component';
+import { ContactComponent } from './contact/contact.component';
 
 @NgModule({
+  imports: [
+    BrowserModule,
+    RouterModule.forRoot([
+      { path: '', component: HomepageComponent },
+      { path: 'about-me', component: AboutMeComponent },
+      { path: 'resume', component: ResumeComponent },
+      { path: 'portfolio', component: PortfolioComponent },
+      { path: 'contact', component: ContactComponent}
+    ])
+  ],
   declarations: [
     AppComponent,
-    TopNavigationBarComponent
-  ],
-  imports: [
-    BrowserModule
+    TopNavigationBarComponent,
+    AboutMeComponent,
+    HomepageComponent,
+    ResumeComponent,
+    PortfolioComponent,
+    ContactComponent
   ],
   providers: [],
   bootstrap: [AppComponent]
 })
+
 export class AppModule { }
